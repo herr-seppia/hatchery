e8fe0634647a401c63f66be0a7c76f309c84777c
Milosz Muszynski <milosz.muszynski@gmail.com>
2022-07-14 16:16:08 +0200

    Add store-restore test

--- tests/boxen.rs
+++ tests/boxen.rs
@@ -1,4 +1,6 @@
 use hatchery::{contract_bytes, Error, World};
+use std::path::PathBuf;
+use dallo::ModuleId;
 
 #[test]
 pub fn box_set_get() -> Result<(), Error> {
@@ -22,18 +24,36 @@ pub fn box_set_get() -> Result<(), Error> {
     Ok(())
 }
 
-#[ignore]
-pub fn box_get() -> Result<(), Error> {
-    let mut world = World::ephemeral()?;
+#[test]
+pub fn box_set_store_restore_get() -> Result<(), Error> {
+    let mut storage_path = PathBuf::new();
+    let first_id: ModuleId;
 
-    let id = world.deploy(hatchery::Env::new(
+    {
+        let mut first_world = World::ephemeral()?;
+
+        first_id = first_world.deploy(hatchery::Env::new(
+            contract_bytes!("box"),
+            first_world.storage_path(),
+        )?);
+
+        first_world.transact(first_id, "set", 0x23)?;
+
+        first_world.storage_path().clone_into(&mut storage_path);
+    }
+
+    let mut second_world = World::new(storage_path);
+
+    let second_id = second_world.deploy(hatchery::Env::new(
         contract_bytes!("box"),
-        world.storage_path(),
+        second_world.storage_path(),
     )?);
 
-    let value: Option<i16> = world.query(id, "get", ())?;
+    assert_eq!(first_id, second_id);
 
-    assert_eq!(value, Some(0x11));
+    let value: Option<i16> = second_world.query(second_id, "get", ())?;
+
+    assert_eq!(value, Some(0x23));
 
     Ok(())
 }
