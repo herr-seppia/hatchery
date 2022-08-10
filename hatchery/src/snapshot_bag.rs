f7843341d13b9498d0b78d556ed605a16def4650
Milosz Muszynski <milosz.muszynski@gmail.com>
2022-08-10 18:42:04 +0200

    Remove accu from snapshot bag

--- hatchery/src/snapshot_bag.rs
+++ hatchery/src/snapshot_bag.rs
@@ -15,9 +15,6 @@ pub struct SnapshotBag {
     ids: Vec<SnapshotId>,
     // we keep top uncompressed snapshot to make save snapshot efficient
     top: SnapshotId,
-    // accu is needed for a series of compressed snapshots depending on each
-    // other
-    accu: SnapshotId,
 }
 
 impl SnapshotBag {
@@ -25,7 +22,6 @@ impl SnapshotBag {
         Self {
             ids: Vec::new(),
             top: SnapshotId::random(),
-            accu: SnapshotId::random(),
         }
     }
     pub fn save_snapshot(
@@ -41,13 +37,15 @@ impl SnapshotBag {
             Ok(0)
         } else {
             let top_snapshot = Snapshot::from_id(self.top, memory_path)?;
-            let accu_snapshot = Snapshot::from_id(self.accu, memory_path)?;
+            let accu_snapshot =
+                Snapshot::from_id(SnapshotId::random(), memory_path)?;
             accu_snapshot.capture(snapshot)?;
             // snapshot saved to accu, accu should be uncompressed, so should be
-            // snapshot compressing snapshot against top
+            // snapshot
+            // compressing snapshot against top
             snapshot.capture_compressed(&top_snapshot, memory_path)?;
             // now snapshot is compressed but accu keeps the uncompressed copy
-            // top is always the last uncompressed
+            // top is always the last uncompressed, so setting it from accu
             top_snapshot.capture(&accu_snapshot)?;
             Ok(self.ids.len() - 1)
         }
@@ -65,7 +63,8 @@ impl SnapshotBag {
         } else if self.ids.len() == (snapshot_index + 1) {
             Snapshot::from_id(self.top, memory_path)?.restore(memory_path)
         } else {
-            let accu_snapshot = Snapshot::from_id(self.accu, memory_path)?;
+            let accu_snapshot =
+                Snapshot::from_id(SnapshotId::random(), memory_path)?;
             let base_snapshot = Snapshot::from_id(self.ids[0], memory_path)?;
             let snapshot = Snapshot::from_id(self.ids[1], memory_path)?;
             snapshot.decompress(&base_snapshot, &accu_snapshot)?;
