15a8b19fffc89528a888cad9dcf84d8b2989cda5
Milosz Muszynski <milosz.muszynski@gmail.com>
2022-09-16 16:01:56 +0200

    Moved counter tests from vm

---
+++ vmx/tests/counter.rs
@@ -0,0 +1,51 @@
+use vmx::{module_bytecode, Error, VM};
+
+#[test]
+fn counter_read() -> Result<(), Error> {
+    let mut vm = VM::ephemeral()?;
+    let id = vm.deploy(module_bytecode!("counter"))?;
+
+    assert_eq!(vm.query::<(), i64>(id, "read_value", ())?, 0xfc);
+
+    Ok(())
+}
+
+#[test]
+fn counter_read_write() -> Result<(), Error> {
+    let mut vm = VM::ephemeral()?;
+    let id = vm.deploy(module_bytecode!("counter"))?;
+
+    {
+        let mut session = vm.session_mut();
+
+        println!("read_value FC");
+        assert_eq!(session.query::<(), i64>(id, "read_value", ())?, 0xfc);
+
+        println!("increment");
+        session.transact::<(), ()>(id, "increment", ())?;
+        session.commit(&id)?; // todo: workaround
+
+        println!("read_value FD");
+        assert_eq!(session.query::<(), i64>(id, "read_value", ())?, 0xfd);
+    }
+
+    // mutable session dropped without committing.
+    // old counter value still accessible.
+
+    println!("read_value FC");
+    assert_eq!(vm.query::<(), i64>(id, "read_value", ())?, 0xfc);
+
+    let mut other_session = vm.session_mut();
+
+    println!("increment");
+    other_session.transact::<(), ()>(id, "increment", ())?;
+    other_session.commit(&id)?; // todo: workaround
+    // let commit_id = other_session.commit();
+
+    // session committed, new value accessible
+
+    println!("read_value FD");
+    assert_eq!(vm.query::<(), i64>(id, "read_value", ())?, 0xfd);
+
+    Ok(())
+}
